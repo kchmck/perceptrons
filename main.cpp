4ef0b289ff996fafc8d5b0ed8c236de3ae7f58db
Mick Koch <mick@kochm.co>
2015-03-15 15:54:48 -0400

    Add initial data file import class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+#include <cstdio>
+#include <fstream>
+
 #include "kernel.hpp"
+#include "optdigits.hpp"
 #include "perceptron.hpp"
 #include "vec.hpp"
 
 int main() {
+    std::fstream trainStream("train.dat");
+
+    if (!trainStream)
+        abort();
+
+    std::fstream develStream("devel.dat");
+
+    if (!develStream)
+        abort();
+
+    std::fstream testStream("test.dat");
+
+    if (!testStream)
+        abort();
+
+    OptDigits od(std::move(trainStream), std::move(develStream),
+                 std::move(testStream));
 }
---
+++ optdigits.hpp
@@ -0,0 +1,84 @@
+#ifndef OPTDIGITS_HPP
+#define OPTDIGITS_HPP
+
+#include <cassert>
+#include <fstream>
+#include <vector>
+
+class DigitData {
+private:
+    static constexpr size_t FEATURES = 64;
+    static constexpr uint32_t DIGITS = 10;
+
+    typedef std::function<void(uint32_t, const Phi &, const Labels &)> IterFn;
+
+public:
+    Phi phi;
+    std::vector<uint32_t> labels;
+
+public:
+    DigitData(std::istream &stream):
+        phi(),
+        labels()
+    {
+        load(stream);
+    }
+
+    void iterDigits(const IterFn &fn) const {
+        Labels binaryLabels(labels.size(), 0);
+
+        for (uint32_t digit = 0; digit < DIGITS; digit += 1) {
+            genLabels(binaryLabels, digit);
+            fn(digit, phi, binaryLabels);
+        }
+    }
+
+private:
+    void genLabels(Labels &binaryLabels, uint32_t digit) const {
+        for (size_t i = 0; i < labels.size(); i += 1)
+            binaryLabels[i] = labels[i] == digit ? +1 : -1;
+    }
+
+    void load(std::istream &stream) {
+        for (;;) {
+            Vec x(FEATURES, 0.0);
+
+            for (size_t i = 0; i < FEATURES; i += 1) {
+                double num;
+                stream >> num;
+
+                if (stream.eof())
+                    return;
+
+                assert(stream);
+
+                x[i] = num;
+
+                char next;
+                assert(stream.get(next));
+                assert(next == ',');
+            }
+
+            uint32_t label;
+            assert(stream >> label);
+
+            phi.emplace_back(std::move(x));
+            labels.push_back(label);
+        }
+    }
+};
+
+class OptDigits {
+public:
+    DigitData train, devel, test;
+
+public:
+    OptDigits(std::istream &&trainStream, std::istream &&develStream,
+              std::istream &&testStream):
+        train(trainStream),
+        devel(develStream),
+        test(testStream)
+    {}
+};
+
+#endif
