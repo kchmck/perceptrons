95bc7895fd6b5eb57b1c8b25fae9b9ee898d86d9
Mick Koch <mick@kochm.co>
2015-03-22 13:45:09 -0400

    Add some docs to digits.hpp

--- digits.hpp
+++ digits.hpp
@@ -8,17 +8,22 @@
 
 #include "vec.hpp"
 
+// Just the number of digits from 0 to 9.
 static constexpr uint32_t DIGITS = 10;
 
+// Loads the data in the format of this project and makes it iterable.
 class DigitData {
 private:
+    // The number of features per example.
     static constexpr size_t FEATURES = 64;
 
     typedef std::function<void(uint32_t, const Phi &, const Labels &)> DigitIterFn;
     typedef std::function<void(const Vec &)> ExampleIterFn;
 
 public:
+    // The current set of examples.
     Phi phi;
+    // The set of labels associated with each digit class.
     std::vector<Labels> digitLabels;
 
 public:
@@ -29,6 +34,8 @@ public:
         load(stream);
     }
 
+    // Iterate over each digit class and call the given function with the set of
+    // examples and the labels for that digit class.
     void iterDigits(const DigitIterFn &fn) const {
         // For each digit, call the function with the corresponding labels
         // vector.
@@ -36,6 +43,8 @@ public:
             fn(digit, phi, digitLabels[digit]);
     }
 
+    // Iterate over all the examples in the given digit class and call the given
+    // function on each feature vector.
     void iterExamples(uint32_t digit, const ExampleIterFn &fn) const {
         for (size_t i = 0; i < phi.size(); i += 1)
             if (digitLabels[digit][i] > 0)
@@ -43,6 +52,7 @@ public:
     }
 
 private:
+    // Load the features and labels from the given stream.
     void load(std::istream &stream) {
         for (;;) {
             Vec x(FEATURES, 0.0);
