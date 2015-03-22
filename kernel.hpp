e194c6aeafac1f64a0f69201dfa97244edff87c5
Mick Koch <mick@kochm.co>
2015-03-22 13:48:55 -0400

    Add some docs to kernel.hpp

--- kernel.hpp
+++ kernel.hpp
@@ -11,12 +11,14 @@
 typedef std::function<double(const Vec &x, const Vec &y)> KernelFn;
 
 namespace kernel {
+    // Get a polynomial kernel of the given degree.
     KernelFn poly(double d) {
         return [=](const Vec &x, const Vec &y) {
             return pow(1 + x.dot(y), d);
         };
     }
 
+    // Get a Gaussian kernel with the given width.
     KernelFn gaussian(double width) {
         double denom = 2 * pow(width, 2);
 
@@ -25,6 +27,7 @@ namespace kernel {
         };
     }
 
+    // Get a normalized kernel of the given kernel function.
     KernelFn normalize(const KernelFn &fn) {
         return [=](const Vec &x, const Vec &y) {
             return fn(x,y) / sqrt(fn(x,x) * fn(y,y));
